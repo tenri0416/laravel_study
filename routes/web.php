6cdb56403663783aca4ea0a094c3b7aaf4b15721
yuki fujimoto <tenriyu0416@gmail.com>
2023-05-09 21:38:53 +0900

    commit

--- routes/web.php
+++ routes/web.php
@@ -29,16 +29,33 @@ Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 Route::post('/contact', [ContactController::class, 'sendMail']);
 Route::get('/contact/complete', [ContactController::class, 'complete'])->name('contact.complete');
 
+//管理画面
 
-Route::get('/admin/blogs', [BlogController::class, 'index'])->name('admin.blogs.index')->middleware('auth');
-Route::get('/admin/blogs/create', [BlogController::class, 'create'])->name('admin.blogs.create')->middleware('auth');
-Route::post('/admin/blogs/create', [BlogController::class, 'store'])->name('admin.blogs.store');
-Route::get('/admin/blogs/edit/{blog}', [BlogController::class, 'edit'])->name('admin.blogs.edit')->middleware('auth');
-Route::patch('/admin/blogs/update/{blog}', [BlogController::class, 'update'])->name('admin.blogs.update');
-Route::delete('/admin/blogs/delete/{blog}', [BlogController::class, 'destroy'])->name('admin.blogs.destroy');
+Route::prefix('/admin')
+    ->name('admin.')
+    ->middleware('admin')
+    ->group(function () {
+        Route::middleware('auth')
+            ->group(function () {
+                Route::resource('/blogs', BlogController::class)->except('show');
+                //ログアウト処理
+                Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+            });
+        Route::middleware('guest')
+            ->group(function () {
+                //認証
+                Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+                Route::post('/login', [AuthController::class, 'login']);
+            });
+    });
+// Route::get('/admin/blogs', [BlogController::class, 'index'])->name('admin.blogs.index')->middleware('auth');
+// Route::get('/admin/blogs/create', [BlogController::class, 'create'])->name('admin.blogs.create')->middleware('auth');
+// Route::post('/admin/blogs/create', [BlogController::class, 'store'])->name('admin.blogs.store');
+// Route::get('/admin/blogs/edit/{blog}', [BlogController::class, 'edit'])->name('admin.blogs.edit')->middleware('auth');
+// Route::patch('/admin/blogs/update/{blog}', [BlogController::class, 'update'])->name('admin.blogs.update');
+// Route::delete('/admin/blogs/delete/{blog}', [BlogController::class, 'destroy'])->name('admin.blogs.destroy');
 
-//ログアウト処理
-Route::post('/admin/logout', [AuthController::class, 'logout'])->name('admin.logout');
+
 
 //ユーザ登録
 
@@ -48,6 +65,3 @@ Route::post('/adim/users', [UserController::class, 'store'])->name('admin.users.
 Route::get('/post/create', [PostController::class, 'create']);
 
 //ログイン用ルーと
-//認証
-Route::get('/admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
-Route::post('/admin/login', [AuthController::class, 'login']);
